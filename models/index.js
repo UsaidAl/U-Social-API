a17dafcd79e246a00ac6c405a3409481a53c9404
UsaidAl <ali_usaid98@hotmail.com>
2024-03-17 22:34:25 +0000

    models index.js imports and exports

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Import the User and Thought models
+const User = require('./User'); 
+const Thought = require('./Thought'); 
+
+// Export the User and Thought models 
+module.exports = { User, Thought };
