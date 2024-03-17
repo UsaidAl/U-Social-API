f6132c631b8eb58b39204fd995e0bf4c913b72ce
UsaidAl <ali_usaid98@hotmail.com>
2024-03-17 22:01:33 +0000

    user models created

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+
+            validate: {
+                validator: function (v) {
+                    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+                },
+                message: "Enter a correct valid email"
+            },
+            required: [true, "Email needed"]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friend_count').get(function () {
+    return `friends: ${this.friends.length}`;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
