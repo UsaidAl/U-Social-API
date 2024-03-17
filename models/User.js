27bed3b78b9c01b05c09bffd777f0cc0e7678718
UsaidAl <ali_usaid98@hotmail.com>
2024-03-17 22:08:27 +0000

    minor changes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
     {
@@ -44,6 +44,6 @@ userSchema.virtual('friend_count').get(function () {
     return `friends: ${this.friends.length}`;
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
